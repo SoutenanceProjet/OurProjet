be9ab3cffb79023cc00351457588fdb5bbbd605a
soutenanceProjet <orensobenas66@gmail.com>
2024-07-06 14:03:26 +0100

    simplify projet

--- src/components/asideRight/Button.jsx
+++
@@ -1,8 +0,0 @@
-import './Button.css';
-
-export default function Button(props) {
-
-    return <>
-                 <div  className="BtnContainer editBtnContainer"> <button type = "submit" className="Btn editBtn">{props.text}</button></div>
-    </>
-}
